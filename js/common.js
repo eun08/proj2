26f534fd8368d4766021d6bfc4a804299e4de894
eun08 <76501271+eun08@users.noreply.github.com>
2021-01-07 05:39:25 +0900

    20210107-0539

--- js/common.js
+++ js/common.js
@@ -9,4 +9,23 @@ $(function(){
         });//animate end
     },2000);//setInterval end
 
+   
+        var sta = 1; //버튼이 햄버거 상태이고 팝업창이 안 열린 상태
+        $('.all_menu').click(function(){
+            $(this).toggleClass('on');//단순디자인 on/off로 변경시에만 이용
+           if (sta == 1) {
+               $(this).addClass('on');
+               $('.menu_box').fadeIn();
+               sta = 2;//상태가 바뀜
+           } else {
+               $(this).removeClass('on');
+               $('.menu_box').fadeOut();
+               sta = 1;//상태가 바뀜
+           }
+        })//click 
+
+
+
+
+
 })//jq
